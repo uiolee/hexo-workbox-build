1027b6f482e9b4d0687d8ab03dd38b3d977766e5
uiolee <22849383+uiolee@users.noreply.github.com>
2023-10-25 00:21:30 +0800

    style: apply recomment and prettier lint

--- index.js
+++ index.js
@@ -1,50 +1,62 @@
 /* global hexo */
 
-'use strict';
+"use strict";
 
-const { injectManifestJson, injectScript } = require('./lib/inject');
-const { manifestJsonGen } = require('./lib/generator');
-const { buildWorkboxFn } = require('./lib/buildWorkbox');
+const { injectManifestJson, injectScript } = require("./lib/inject");
+const { manifestJsonGen } = require("./lib/generator");
+const { buildWorkboxFn } = require("./lib/buildWorkbox");
 
-hexo.config.workboxBuild = Object.assign({
-  enable: false,
+hexo.config.workboxBuild = Object.assign(
+  {
+    enable: false,
 
-  buildWorkbox: true,
-  swDest: '',
-  generateSWOptionPath: '',
-  injectManifestOptionPath: '',
+    buildWorkbox: true,
+    swDest: "",
+    generateSWOptionPath: "",
+    injectManifestOptionPath: "",
 
-  injectScript: true,
-  scriptNjkPath: '',
+    injectScript: true,
+    scriptNjkPath: "",
 
-  ManifestJsonSrc: '',
-  ManifestJsonDest: '',
+    ManifestJsonSrc: "",
+    ManifestJsonDest: "",
 
-  injectManifestJson: false
-
-}, hexo.config.workboxBuild);
+    injectManifestJson: false,
+  },
+  hexo.config.workboxBuild,
+);
 
 const opts = hexo.config.workboxBuild;
 
-if (!opts.enable || (!hexo.env.cmd.startsWith('g') && !hexo.env.cmd.startsWith('d') && !hexo.env.cmd.startsWith('s'))) {
-  hexo.log.info('hexo-workbox-build will not run.');
+if (
+  !opts.enable ||
+  (!hexo.env.cmd.startsWith("g") &&
+    !hexo.env.cmd.startsWith("d") &&
+    !hexo.env.cmd.startsWith("s"))
+) {
+  hexo.log.info("hexo-workbox-build will not run.");
 } else {
-
   if (opts.buildWorkbox) {
-    hexo.extend.filter.register('before_exit', buildWorkboxFn);
+    hexo.extend.filter.register("before_exit", buildWorkboxFn);
   }
 
   if (opts.injectScript) {
-    hexo.extend.injector.register('body_end', injectScript(opts, hexo), 'default');
+    hexo.extend.injector.register(
+      "body_end",
+      injectScript(opts, hexo),
+      "default",
+    );
   }
 
   if (opts.ManifestJsonSrc && opts.ManifestJsonDest) {
-    hexo.extend.generator.register('manifest_json', manifestJsonGen);
+    hexo.extend.generator.register("manifest_json", manifestJsonGen);
   }
 
   if (opts.injectManifestJson && opts.ManifestJsonDest) {
-    hexo.extend.injector.register('head_end', injectManifestJson(opts), 'default');
+    hexo.extend.injector.register(
+      "head_end",
+      injectManifestJson(opts),
+      "default",
+    );
   }
 }
-
-
